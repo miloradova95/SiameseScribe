18bd4ca1ebeaa486838d5a2248cef236710dcca5
rapha <cc231040@fhstp.ac.at>
2026-04-20 21:39:12 +0200

    added get_patches_by_file_name method

--- services/backend/services/patch_service.py
+++ services/backend/services/patch_service.py
@@ -12,3 +12,6 @@ def get_by_id(session: Session, patch_id: int) -> Patch | None:
 
 def get_by_image_id(session: Session, image_id: int) -> list[Patch]:
     return session.exec(select(Patch).where(Patch.source_image_id == image_id)).all()
+
+def get_by_file_name(session: Session, file_name: str) -> Patch | None:
+    return session.exec(select(Patch).where(Patch.file_path.contains(file_name))).first()
